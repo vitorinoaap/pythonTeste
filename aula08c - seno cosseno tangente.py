8fc8be54071bcb94a1af2bcf3de0c0d630cf1940
vitorinoaap <bacamarte.rj@gmail.com>
2023-12-28 11:03:38 -0300

    print(dir(nome do modulo importado))

--- aula08c - seno cosseno tangente.py
+++ aula08c - seno cosseno tangente.py
@@ -5,3 +5,4 @@ angulo = float(input('Digite o angulo: '))
 
 print('O seno é {:.2f}, o cosseno é {:.2f} e a tangente é {:.2f}'.format(math.sin(math.radians(angulo)), math.cos(math.radians(angulo)), math.tan(math.radians(angulo))))
 
+# print(dir(math))
